6b7b14fc5024bfe5398cbc6ff0921d03915def79
Menno Drescher <menno@cbaconsult.eu>
2025-01-31 08:35:47 +0100

    Release notes today

--- yukina.config.ts
+++ yukina.config.ts
@@ -2,9 +2,9 @@ import I18nKeys from "./src/locales/keys";
 import type { Configuration } from "./src/types/config";
 
 const YukinaConfig: Configuration = {
-  title: "Yukina",
-  subTitle: "Yukina Template Demo Site",
-  brandTitle: "Yukina",
+  title: "CBA",
+  subTitle: "CBA Template Demo Site",
+  brandTitle: "CBA",
 
   description: "Demo Site",
 
@@ -27,37 +27,37 @@ const YukinaConfig: Configuration = {
     },
     {
       nameKey: I18nKeys.nav_bar_github,
-      href: "https://github.com/WhitePaper233/yukina",
+      href: "https://github.com/mdresch/yukina",
     },
   ],
 
-  username: "WhitePaper 白芷",
+  username: "Menno Drescher",
   sign: "Ad Astra Per Aspera.",
-  avatarUrl: "https://s2.loli.net/2025/01/25/FPpTrQSezM8ivbl.webp",
+  avatarUrl: "https://i.ibb.co/4w5f1cY5/SCEV-placeholder.jpg",
   socialLinks: [
     {
       icon: "line-md:github-loop",
-      link: "https://github.com/WhitePaper233",
+      link: "https://github.com/mdresch",
     },
     {
       icon: "mingcute:bilibili-line",
-      link: "https://space.bilibili.com/22433608",
+      link: "https://space.bilibili.com/",
     },
     {
       icon: "mingcute:netease-music-line",
-      link: "https://music.163.com/#/user/home?id=125291648",
+      link: "https://music.163.com/#/user/home",
     },
   ],
 
   banners: [
-    "https://s2.loli.net/2025/01/25/PBvHFjr5yDu6t4a.webp",
-    "https://s2.loli.net/2025/01/25/6bKcwHZigzlM4mJ.webp",
-    "https://s2.loli.net/2025/01/25/H9WgEK6qNTcpFiS.webp",
-    "https://s2.loli.net/2025/01/25/njNVtuUMzxs81RI.webp",
-    "https://s2.loli.net/2025/01/25/tozsJ8QHAjFN3Mm.webp",
-    "https://s2.loli.net/2025/01/25/Pm89OveZq7NWUxF.webp",
-    "https://s2.loli.net/2025/01/25/UCYKvc1ZhgPHB9m.webp",
-    "https://s2.loli.net/2025/01/25/JjpLOW8VSmufzlA.webp",
+    "https://i.ibb.co/5DjgD77/Firefly-Self-Charging-Electric-Vehicle-whereby-the-wheel-rotation-and-solar-but-also-rergenerative-b.jpg",
+    "https://i.ibb.co/ycTzWvsG/Firefly-Self-Charging-Electric-Vehicle-whereby-the-wheel-rotation-and-solar-but-also-rergenerative-b.jpg",
+    "https://i.ibb.co/9HH5Y8jV/Firefly-Self-Charging-Electric-Vehicle-whereby-the-wheel-rotation-and-solar-but-also-rergenerative-b.jpg",
+    "https://i.ibb.co/JFn2Hvzv/electric-vehicle-charging-battery-pack-from-kinetic-energy-production-movement-of-the-car.png",
+    "https://i.ibb.co/zHfbK5Ss/Adobe-Stock-1024994346-Preview.jpg",
+    "https://i.ibb.co/PZsvkyyG/Adobe-Stock-979483004-Preview.jpg",
+    "https://i.ibb.co/qSj3X0q/Adobe-Stock-962733580-Preview.jpg",
+    "https://i.ibb.co/tp8mP0RK/Adobe-Stock-673077217-Preview.jpg",
   ],
 
   slugMode: "HASH", // 'RAW' | 'HASH'
